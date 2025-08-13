595ca251e43e5c1c325c3c2662ca422bca38fc00
kagilinn <kagilinn@gmail.com>
2025-08-13 10:26:04 +0900

    PowerShell に git 系の関数や zip 系の alias を追加.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,13 @@
 Set-PSReadLineOption -EditMode Emacs
+
+function ga    { git add                                    @args }
+function gf    { git fetch                                  @args }
+function gloga { git log --graph --oneline --decorate --all @args }
+function gst   { git status                                 @args }
+function gsta  { git stash push                             @args }
+function gstl  { git stash list                             @args }
+function gstp  { git stash pop                              @args }
+function gsw   { git switch                                 @args }
+
+New-Alias 'unzip' 'Expand-Archive'
+New-Alias 'zip'   'Compress-Archive'
